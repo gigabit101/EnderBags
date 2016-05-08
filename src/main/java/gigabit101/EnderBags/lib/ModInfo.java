94dced577b4433684832f78fb10eb9cd06494e4d
gigabit101 <thegigabit101@gmail.com>
2016-05-08 14:28:59 +0100

    Version bump

--- src/main/java/gigabit101/EnderBags/lib/ModInfo.java
+++ src/main/java/gigabit101/EnderBags/lib/ModInfo.java
@@ -8,5 +8,5 @@ public class ModInfo
 {
     public static final String MOD_NAME = "EnderBags";
     public static final String MOD_ID = "EnderBags";
-    public static final String MOD_VERSION = "1.0.1";
+    public static final String MOD_VERSION = "1.0.2";
 }
